5be6a32fead5470d8cae59ecb061105819d4b558
Piotr Bartosiewicz <p.bartosiewi@partner.samsung.com>
2014-12-22 12:01:20 +0100

    Debug utility class: SameThreadGuard
    [Bug/Feature]   A utility for checking the code against invalid assumes
                    about synchronization needs.
    [Cause]         N/A
    [Solution]      N/A
    [Verification]  Run tests
    
    Change-Id: I880f9c334d8d461e2472052db5244d9f5eab1bb8

---
+++ common/utils/same-thread-guard.hpp
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Contact: Piotr Bartosiewicz <p.bartosiewi@partner.samsung.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+/**
+ * @file
+ * @author  Piotr Bartosiewicz (p.bartosiewi@partner.samsung.com)
+ * @brief   Same thread guard
+ */
+
+#ifndef COMMON_UTILS_SAME_THREAD_GUARD_HPP
+#define COMMON_UTILS_SAME_THREAD_GUARD_HPP
+
+#ifndef NDEBUG
+#define ENABLE_SAME_THREAD_GUARD
+#endif
+
+#ifdef ENABLE_SAME_THREAD_GUARD
+#include <atomic>
+#include <cassert>
+#endif
+
+namespace vasum {
+namespace utils {
+
+/**
+ * Same thread guard.
+ * There are two purposes of this guard:
+ * - reports invalid assumptions about synchronization needs (only in debug builds)
+ * - acts as an annotation in the source code about the thread safety
+ *
+ * Usage example:
+ * ASSERT_SAME_THREAD(workerThreadGuard);
+ */
+class SameThreadGuard {
+public:
+#ifdef ENABLE_SAME_THREAD_GUARD
+#   define ASSERT_SAME_THREAD(g) assert(g.check())
+    SameThreadGuard();
+
+    /**
+     * On the first call it remembers the current thread id.
+     * On the next call it verifies that current thread is the same as before.
+     */
+    bool check();
+
+    /**
+     * Reset thread id
+     */
+    void reset();
+
+private:
+    std::atomic<unsigned int> mThreadId;
+
+#else // ENABLE_SAME_THREAD_GUARD
+#   define ASSERT_SAME_THREAD(g)
+    bool check() {return true;}
+    void reset() {}
+#endif // ENABLE_SAME_THREAD_GUARD
+};
+
+} // namespace utils
+} // namespace vasum
+
+
+#endif // COMMON_UTILS_SAME_THREAD_GUARD_HPP
---
+++ tests/unit_tests/utils/ut-same-thread-guard.cpp
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Contact: Piotr Bartosiewicz <p.bartosiewi@partner.samsung.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+
+/**
+ * @file
+ * @author  Piotr Bartosiewicz (p.bartosiewi@partner.samsung.com)
+ * @brief   Unit tests of same thread guard
+ */
+
+#include "config.hpp"
+#include "ut.hpp"
+
+#include "utils/same-thread-guard.hpp"
+#include <thread>
+
+#ifdef ENABLE_SAME_THREAD_GUARD
+
+BOOST_AUTO_TEST_SUITE(SameThreadGuardSuite)
+
+using namespace vasum::utils;
+
+BOOST_AUTO_TEST_CASE(SimpleTest)
+{
+    SameThreadGuard guard;
+    BOOST_CHECK(guard.check());
+    BOOST_CHECK(guard.check());
+    guard.reset();
+    BOOST_CHECK(guard.check());
+    BOOST_CHECK(guard.check());
+}
+
+BOOST_AUTO_TEST_CASE(ThreadTest)
+{
+    SameThreadGuard guard;
+
+    std::thread([&] {
+        BOOST_CHECK(guard.check());
+    }).join();
+
+    BOOST_CHECK(!guard.check());
+    BOOST_CHECK(!guard.check());
+
+    guard.reset();
+    BOOST_CHECK(guard.check());
+
+    std::thread([&] {
+        BOOST_CHECK(!guard.check());
+    }).join();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif // ENABLE_SAME_THREAD_GUARD
