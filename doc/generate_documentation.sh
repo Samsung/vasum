eb997958fb16998ac0a5a2c659d3a69aa1922e92
Lukasz Kostyra <l.kostyra@samsung.com>
2015-07-23 08:06:23 +0200

    Modify generate_documentation.sh
    [Feature]       N/A
    [Cause]         N/A
    [Solution]      N/A
    [Verification]  Run script, output html dir should be in doc directory.
                    Run without doxygen installed, should exit with an error message.
    
    Change-Id: Ibfc0026da771ba1415f034ce8e81eaeb1deab0c2

--- doc/generate_documentation.sh
+++ doc/generate_documentation.sh
@@ -2,4 +2,19 @@
 
 DOC_CFG="doxygen.cfg"
 
+# Check if doxy is visible
+echo -n "Checking for doxygen... "
+EXE_PATH=$(which doxygen)
+if [ ! -x "$EXE_PATH" ] ; then
+    echo "NOT FOUND, EXITING"
+else
+    echo "FOUND"
+fi
+
+# Change pwd to script dir, to keep the paths consistent
+pushd . > /dev/null
+cd "$(dirname "${BASH_SOURCE[0]}" )"
+
 doxygen "$DOC_CFG"
+
+popd > /dev/null
